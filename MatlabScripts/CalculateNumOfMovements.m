0c42bcb19adbd4bcb806e08ef487517312abac05
Lucklyric <lucklyric@gmail.com>
2015-04-27 00:57:36 -0600

    1. Update New PathLength and Num of movements matlab scripts

--- MatlabScripts/CalculateNumOfMovements.m
+++ MatlabScripts/CalculateNumOfMovements.m
@@ -1,19 +1,13 @@
-function [ numberOfMovements ] = CalculateNumOfMovements( rawObservation )
+function [ numberOfMovements,meansArray,tangVel ] = CalculateNumOfMovements( rawObservation )
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-numberOfMovements = 0;
-startmovement = 1;
-tmpIndex= 0;
-while startmovement < length(rawObservation)
-    tangVel = sqrt(rawObservation(startmovement:end,1).^2 + rawObservation(startmovement:end,2).^2);
-    startmovement = find(tangVel >0.05*max(tangVel),1)+tmpIndex;
-    passedhalf = find(tangVel > 0.5 * max(tangVel),1);
-    endmovement = find(tangVel(passedhalf:end,:) < 0.05*max(tangVel),1) + passedhalf - 1+tmpIndex; 
-    startmovement = endmovement;
-    tmpIndex = endmovement;
-    display(startmovement);
-    numberOfMovements = numberOfMovements + 1;
-end
+
+tangVel = sqrt(rawObservation(1:end,1).^2 + rawObservation(1:end,2).^2+rawObservation(1:end,3).^2);
+means = mean(tangVel);
+[a,b]=findpeaks(tangVel);
+numberOfMovements = length(find(a>means));
+
+meansArray(1:length(tangVel))=means;
 
 end
 
